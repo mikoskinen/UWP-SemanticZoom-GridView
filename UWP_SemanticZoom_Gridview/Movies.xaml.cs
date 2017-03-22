b11c482ee8d6f7de69825eb4fdb8c9c9949b4af8
Mikael Koskinen <mijapkos@gmail.com>
2017-03-22 18:39:27 +0200

    Sample app

---
+++ UWP_SemanticZoom_Gridview/Movies.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using UWP_SemanticZoom_Gridview.Common;
+
+namespace UWP_SemanticZoom_Gridview
+{
+    public sealed partial class Movies
+    {
+        public Movies()
+        {
+            this.InitializeComponent();
+
+            var viewModel = new MoviesPageViewModel();
+            this.DataContext = viewModel;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var collectionGroups = groupedItemsViewSource.View.CollectionGroups;
+            ((ListViewBase)this.Zoom.ZoomedOutView).ItemsSource = collectionGroups;
+        }
+    }
+}
